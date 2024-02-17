8a1ceebd1ef39c6bbd8841b50e1f841c2448fbf7
James-wasonga <jameswasonga22@gmail.com>
2024-02-17 10:58:28 +0300

    partial spliting of phphp files

---
+++ admin/manage-categories.php
@@ -0,0 +1,8 @@
+<?php include("partials/menu.php");?>
+<div class="main-content">
+    <div class="wrapper">
+  <strong>Manage Category</strong>
+</div>
+</div>
+
+<?php include("partials/footer.php");?>
--- admin/partials/menu.php
+++ admin/partials/menu.php
@@ -11,11 +11,11 @@
    <div class="menu text-center">
     <div class="wrapper">
     <ul>
-     <li><a href="">Home</a></li> 
-     <li><a href="">Admin</a></li>
-     <li><a href="">Category</a></li>   
-     <li><a href="">food</a></li> 
-     <li><a href="">order</a></li>   
+     <li><a href="index.php">Home</a></li> 
+     <li><a href="manage-admin.php">Admin</a></li>
+     <li><a href="manage-categories.php">Category</a></li>   
+     <li><a href="manage-food.php">food</a></li> 
+     <li><a href="manage-order,php">order</a></li>   
 
 
 
